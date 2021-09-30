480d23e98712fcc6dc4c8a04f9dc27e58b188429
Stewart MacLeman <stewart_macleman@hotmail.com>
2021-09-30 18:21:32 +0100

    Basic layout and styles created.

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
-console.log("This is a set-up test!");
+"use strict";
+
+// Global variables. -----------------------------------------------------
+let addAbbrevButton = document.getElementById("subBut");
+let modelCoverDiv = document.querySelector(".modelCover");
+let addAbbrevModelDiv = document.querySelector(".addAbbrevModel");
+let submitCancelButton = document.getElementById("subCancel");
+
+
+// Show the model to add an abbreviation. ---------------------------
+addAbbrevButton.addEventListener("click", showAddModel);
+
+function showAddModel(){
+  modelCoverDiv.classList.remove("hide");
+  addAbbrevModelDiv.classList.remove("hide");
+}
+// Cancel the submit and close the model. ----------------------------
+submitCancelButton.addEventListener("click", closeAddModel);
+
+function closeAddModel(){
+  modelCoverDiv.classList.add("hide");
+  addAbbrevModelDiv.classList.add("hide");
+}
