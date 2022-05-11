17a342f212b8616da648af52d220d33205ddafb0
Stewart MacLeman <stewart_macleman@hotmail.com>
2022-05-11 16:42:28 +0100

    Styling updates.

--- server.js
+++ server.js
@@ -69,6 +69,6 @@ app.post("/delete", (req, res) => {
   })
 });
 
-app.listen(3000, () => {
-  console.log("The server is running on port 3000.");
+app.listen(5000, () => {
+  console.log("The server is running on port 5000.");
 })
